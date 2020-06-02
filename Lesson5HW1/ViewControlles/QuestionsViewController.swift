fad5e2102213dfcad577655b75ba28567baedd4b
Vladimir <vaksakalovbot@yandex.ru>
2020-06-02 16:23:09 +0300

    Update algorithm

--- Lesson5HW1/ViewControlles/QuestionsViewController.swift
+++ Lesson5HW1/ViewControlles/QuestionsViewController.swift
@@ -152,16 +152,13 @@ extension QuestionsViewController {
             performSegue(withIdentifier: "resultSegue", sender: nil)
         }
     }
-    
-    
-    /*
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+        
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "resultSegue" {
+            let resultVC = segue.destination as! ResultsViewController
+            resultVC.answersChoosen = answersChoosen
+        }
     }
-    */
 }
 
 
--- Lesson5HW1/ViewControlles/ResultsViewController.swift
+++ Lesson5HW1/ViewControlles/ResultsViewController.swift
@@ -14,11 +14,57 @@ class ResultsViewController: UIViewController {
     // 2. Определить наиболее часто встречающийся тип животного
     // 3. Отобразить результаты на экране
     // 4. Избавится от кнопки back
-
+    
+    @IBOutlet var resultLabel: UILabel!
+    @IBOutlet var textLabel: UILabel!
+    
+    var answersChoosen: [Answer]!
+    var counterTypes : [AnimalType : Int] = [
+        .cat : 0,
+        .dog : 0,
+        .rabbit : 0,
+        .turtle : 0
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        navigationItem.hidesBackButton = true
+        updateUI()
+    }
+    
+    private func updateUI() {
+        let resultAnimalType = findResultAnimalType()
+        switch resultAnimalType {
+        case .cat:
+            resultLabel.text = "Вы - 🐱"
+        case .dog:
+            resultLabel.text = "Вы - 🐶"
+        case .turtle:
+            resultLabel.text = "Вы - 🐢"
+        case .rabbit:
+            resultLabel.text = "Вы - 🐰"
+        }
+        textLabel.text = resultAnimalType.definition
+    }
+    
+    private func findResultAnimalType() -> AnimalType {
+        answersChoosen.forEach { (answer) in
+            counterTypes[answer.type]! += 1
+        }
+        
+        var resultAnimalType = counterTypes.first!.key
+        var maxValue = counterTypes.first!.value
+        
+        for (key, value) in counterTypes {
+            if value > maxValue {
+                maxValue = value
+                resultAnimalType = key
+            }
+            print("\(key): \(value)")
+        }
+        print("Result is \(resultAnimalType)")
+        
+        return resultAnimalType
     }
     
     deinit {
